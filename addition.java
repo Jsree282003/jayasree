66925bd2e2d14fd5f4b5f415de04f188d38fbb15
Jsree282003 <115019226+Jsree282003@users.noreply.github.com>
2022-10-04 15:17:31 +0100

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,14 @@
+package addition;
+
+public class addition {
+
+	public static void main(String[] args) {
+	   System.out.println("Enter two numbers");
+	   int first = 100;
+	   int second = 200;
+	   System.out.println(first + " " + second);
+	   int sum = first + second;
+	   System.out.println("The sum is: " + sum);
+	   }
+
+}
