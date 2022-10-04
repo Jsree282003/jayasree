92d1587d72a8fb423ed7cc0c18a7abcfb545d0f5
Jsree282003 <115019226+Jsree282003@users.noreply.github.com>
2022-10-04 17:50:31 +0100

    Add files via upload

---
+++ CastingExercise.java
@@ -0,0 +1,20 @@
+
+public class CastingExercise {
+
+	public static void main(String[] args) {
+	byte b=10;
+	
+	int i=b;
+	
+	byte c=i;
+	
+	byte d=(byte)i;
+	
+	byte x=10;
+	byte y=20;
+	
+	byte sum=x*y;
+
+	}
+
+}
