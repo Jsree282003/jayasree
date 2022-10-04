9f896e7d569b979f4e4d2065c343788a372aa57b
Jsree282003 <115019226+Jsree282003@users.noreply.github.com>
2022-10-04 17:52:55 +0100

    Add files via upload

---
+++ SwapTwoNumbersExercise.java
@@ -0,0 +1,23 @@
+
+public class SwapTwoNumbersExercise {
+
+	public static void main(String[] args) {
+	
+		float first = 2.50f, second = 4.50f;
+		
+		System.out.println("--Before swap--");
+		System.out.println("First number = " + first);
+		System.out.println("Second number = " + second);
+		
+		float temporary = first;
+		
+		first = second;
+		 second = temporary;
+		 
+		 System.out.println("--After swap--");
+		 System.out.println("First number = " + first);
+		 System.out.println("Secon number = " + second);
+
+	}
+
+}
