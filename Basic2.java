fe248d827345f528ef42bc46187b401e770361c9
Jsree282003 <115019226+Jsree282003@users.noreply.github.com>
2022-10-04 15:18:14 +0100

    Add files via upload

---
+++ Basic2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Basic2 {
+
+	public static void main(String[] args) {
+	   Scanner scan=new Scanner(system.in);
+	   
+	   system.out.println("Enter value for i...:");
+	   
+	   int i=scan.nextInt();
+	   System.out.println("Enter value for j...:");
+	   
+	   int j=scan.nextInt();
+	   
+	   int mul=i*j;
+	   
+	   System.out.println("The Multiplication of i and j is...:"+mul);
+	   
+	}
+
+}
