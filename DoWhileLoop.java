be7223137d38e21b8fccab96f21d27e53a21542f
Jsree282003 <115019226+Jsree282003@users.noreply.github.com>
2022-10-04 17:55:08 +0100

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,19 @@
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		 int x = 1;
+		 
+		 do {
+			 System.out.println("value of x : " + x );
+			 x++;
+			 System.out.println("\n");
+		 }while( x < 11 );
+	  }
+}
+		 
+		 
+
+	
+
+
