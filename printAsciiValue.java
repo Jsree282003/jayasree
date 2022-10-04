00a684ad9c63a5a3e122bf8492d728af5d7b4a56
Jsree282003 <115019226+Jsree282003@users.noreply.github.com>
2022-10-04 17:51:03 +0100

    Add files via upload

---
+++ printAsciiValue.java
@@ -0,0 +1,14 @@
+package printAsciiValue;
+
+public class printAsciiValue {
+
+	public static void main(String[] args) {
+	    char ch = 'H';
+	    int ascii = ch;
+	    int castAscii = (int) ch;
+	    System.out.println("The ASCII value of " + " is: " + ascii);
+	    system.out.println("The ASCII value of " + ch + " is: " + castAscii);
+
+	}
+
+}
