af4612d73c44bb48bee0ce69f50447c9eec29509
Jsree282003 <115019226+Jsree282003@users.noreply.github.com>
2022-10-04 17:49:45 +0100

    Add files via upload

---
+++ printAnumber.java
@@ -0,0 +1,16 @@
+package printAnumber;
+
+public class printAnumber {
+
+	public static void main(String[] args) {
+	   Scanner reader = new Scanner(System.in);
+	   System.out.println("Enter a number:");
+	   
+	   int number = reader.nextInt();
+	   
+	   System.out.println("You entered: " + number);
+	   
+
+	}
+
+}
